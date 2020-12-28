af8b03615d8d71ab025b2e2ec7d8c25d4c1821ad
Marhamat <marhamatbagirov@gmail.com>
2020-12-28 12:36:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to our game center")
+name=str(input("What is your name? "))
+age=int(input("How old are you? "))
+
+if age >= 18:
+ print("Please book your seat and start to play")
+else:
+ print("Sorry, your age is not enough to enter game center!")
